3036e6c8cebcefd593715254d0e1feb40b6978df
Rabah SELLAH <rabahsella@gmail.com>
2025-02-16 19:26:33 +0100

    Tests - Scenario : repository.update & Scenario : repository.findById

--- src/webinars/adapters/webinar-repository.prisma.int.test.ts
+++ src/webinars/adapters/webinar-repository.prisma.int.test.ts
@@ -80,7 +80,72 @@ describe('PrismaWebinarRepository', () => {
       });
     });
   });
+
+  describe('Scenario : repository.findById', () => {
+    it('should find a webinar by id', async () => {
+      // ARRANGE
+      await prismaClient.webinar.create({
+        data: {
+          id: 'webinar-id',
+          organizerId: 'organizer-id',
+          title: 'Webinar title',
+          startDate: new Date('2022-01-01T00:00:00Z'),
+          endDate: new Date('2022-01-01T01:00:00Z'),
+          seats: 100,
+        },
+      });
+
+      // ACT
+      const foundWebinar = await repository.findById('webinar-id');
+
+      // ASSERT
+      expect(foundWebinar).toEqual(expect.objectContaining({
+        id: 'webinar-id',
+        organizerId: 'organizer-id',
+        title: 'Webinar title',
+      }));
+    });
+  });
+
+  describe('Scenario : repository.update', () => {
+    it('should update a webinar', async () => {
+      // ARRANGE
+      await prismaClient.webinar.create({
+        data: {
+          id: 'webinar-id',
+          organizerId: 'organizer-id',
+          title: 'Old title',
+          startDate: new Date('2022-01-01T00:00:00Z'),
+          endDate: new Date('2022-01-01T01:00:00Z'),
+          seats: 100,
+        },
+      });
+
+      const updatedWebinar = new Webinar({
+        id: 'webinar-id',
+        organizerId: 'organizer-id',
+        title: 'Updated title',
+        startDate: new Date('2022-01-01T00:00:00Z'),
+        endDate: new Date('2022-01-01T01:00:00Z'),
+        seats: 150,
+      });
+
+      // ACT
+      await repository.update(updatedWebinar);
+
+      // ASSERT
+      const maybeWebinar = await prismaClient.webinar.findUnique({
+        where: { id: 'webinar-id' },
+      });
+      expect(maybeWebinar).toEqual(expect.objectContaining({
+        id: 'webinar-id',
+        title: 'Updated title',
+        seats: 150,
+      }));
+    });
+  });
 });
+
 
 
 
